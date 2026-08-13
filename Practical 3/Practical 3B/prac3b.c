f442281e8961a986138dee3159ef30a515b2f601
Naiteek11 <naiteekpb@gmail.com>
2026-08-13 10:35:57 +0530

    Update prac3b.c

--- Practical 3/Practical 3B/prac3b.c
+++ Practical 3/Practical 3B/prac3b.c
@@ -1,5 +1,24 @@
-// title: 
 #include <stdio.h>
-int main(){
-  return 0;
+#include <string.h>
+int main() {
+    char *arr[] = {"Apple", "Banana", "Cherry", "Coconut", "Blueberry", "Tomato"};
+    int f=0;
+    char target[100];
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Give a target string to search: ");
+    scanf("%s", target);
+    
+    for(int i=0;i<size;i++){
+        if(strcmp(arr[i], target) == 0) {
+            f = f + i + 1;
+            break;
+        }
+        
+    }
+    if(f>0){
+        printf("tareget found at position: %d", f);
+    }else{
+        printf("Element not found.");
+    }
+    return 0;
 }
